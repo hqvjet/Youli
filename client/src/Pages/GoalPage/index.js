cf7e76a354ae983f14a9815f84795c1b3b3ff6fa
Ralph Hoang <105470839+hqvjet@users.noreply.github.com>
2022-12-23 14:31:05 +0700

    Delete client directory

--- client/src/Pages/GoalPage/index.js
+++
@@ -1,9 +0,0 @@
-import styles from "./GoalPage.module.css"
-
-function GoalPage() {
-    return (
-        <h1>Goal Page</h1>
-    )
-}
-
-export default GoalPage;
--- client/src/api/ProfileAPI.js
+++
@@ -1,6 +0,0 @@
-import {PROFILE} from "./options";
-
-export const uploadData = async data => {
-    // console.log(data)
-    return (await PROFILE.post('/save', data)).data
-}
